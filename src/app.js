7d2fa92c2fe603c58a8310226aed3ed88e048bcf
Melisa-boop <melicabagnero@hotmail.com>
2021-05-06 12:39:31 -0300

    mi first commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // Librerias
 const path = require('path');
+const bodyParser =require ('body-parser');
 
 // Express
 const express = require('express');
@@ -7,16 +8,16 @@ const app = express();
 const methodOverride = require('method-override')
 
 // Routers
-const indexRouter = require('./routers/index');
-const detailRouter= require('./routers/detail')
 
+const apiRouter= require ('./routers/apiRouter')
 
 // Configuramos el motor de vistas y la carpeta de vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Usamos methodOverride para poder implementar los métodos PUT y DELETE
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(methodOverride('_method'))
@@ -25,8 +26,7 @@ app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Seteamos los routers
-app.use('/',indexRouter)
-app.use('/edit',detailRouter)
+app.use('/api',apiRouter);
 
 // Levantamos el server en el puerto indicado en la variable de entorno PORT, o en el puerto 3000 si no hay nada ahí.
 // Más info en: https://stackoverflow.com/questions/18864677/what-is-process-env-port-in-node-js
