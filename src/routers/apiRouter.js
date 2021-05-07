270851be635e4c934f171aa19e5c8de50c489b8e
Melisa-boop <melicabagnero@hotmail.com>
2021-05-06 22:05:17 -0300

    modifcacion del apicontroller

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -5,8 +5,8 @@ const apiController = require('../controllers/apiController');
 //ACTORES
 router.get('/actores',apiController.listadoActores);//lista
 router.post('/crear',apiController.creacion);//crear 
-router.delete('/actores/:id',apiController.delete);//eliminar 
-router.put('/actores/:id', apiController.update);//editar
+router.post('/actores/:id',apiController.delete);//eliminar 
+router.post('/actores/:id', apiController.update);//editar
 
 router.get('/actores/:id',apiController.detalle);//por ID y su detalle
 router.get('/actores/search',apiController.search);//busqueda por ID
