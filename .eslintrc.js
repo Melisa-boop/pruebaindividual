f6854d553f52d6f5d3b663a0bd85e8ba11b9fc47
joscabrera <jose.cabrera@mercadolibre.com>
2021-05-08 14:06:53 -0300

    added eslint & guide styles config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+  module.exports = {
+	env: {
+		browser  : true,
+		commonjs : true,
+		es6      : true,
+	},
+	extends: [
+		`airbnb-base`,
+	],
+	globals: {
+		Atomics           : `readonly`,
+		SharedArrayBuffer : `readonly`,
+	},
+	parserOptions: {
+		ecmaVersion: 11,
+	},
+	rules: {
+		'consistent-return' : `error`,
+		'brace-style'       : [`error`, `allman`],
+		'space-in-parens'   : [`error`, `always`],
+		'linebreak-style'   : [`off`],
+		indent              : [`error`, `tab`],
+		'no-tabs'           : [`error`, { allowIndentationTabs: true }],
+		quotes              : [`error`, `backtick`],
+		'key-spacing'       : [`error`, {
+			align: {
+				beforeColon : true,
+				afterColon  : true,
+				on          : `colon`,
+			},
+		}],
+	},
+};
--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
 // Librerias
-const path = require('path');
-const bodyParser =require ('body-parser');
+const path = require( `path` );
+const bodyParser = require( `body-parser` );
 
 // Express
-const express = require('express');
+const express = require( `express` );
+
 const app = express();
-const methodOverride = require('method-override')
+const methodOverride = require( `method-override` );
 
 // Routers
 
-const apiRouter= require ('./routers/apiRouter')
+const apiRouter = require( `./routers/apiRouter` );
 
 // Configuramos el motor de vistas y la carpeta de vistas
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set( `view engine`, `ejs` );
+app.set( `views`, path.join( __dirname, `views` ) );
 
 // Usamos methodOverride para poder implementar los métodos PUT y DELETE
-app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use( bodyParser.json() );
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.json() );
 
-app.use(methodOverride('_method'))
+app.use( methodOverride( `_method` ) );
 
 // Seteamos la carpeta public con contenido estático
-app.use(express.static(path.join(__dirname, '../public')));
+app.use( express.static( path.join( __dirname, `../public` ) ) );
 
 // Seteamos los routers
-app.use('/api',apiRouter);
+app.use( `/api`, apiRouter );
 
 // Levantamos el server en el puerto indicado en la variable de entorno PORT, o en el puerto 3000 si no hay nada ahí.
 // Más info en: https://stackoverflow.com/questions/18864677/what-is-process-env-port-in-node-js
-app.listen(process.env.PORT || 3000,function() {
-    console.log("Server running...")
-    console.log("http://localhost:3000/")
-})
+app.listen( process.env.PORT || 3000, () =>
+{
+	console.log( `Server running...` );
+	console.log( `http://localhost:3000/` );
+} );
