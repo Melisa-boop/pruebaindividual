19749b637bf0873885dbd81ba53c81a84b41be47
techjosec <tec.josec@gmail.com>
2021-05-17 23:31:40 -0300

    added DB config in .env

--- README.md
+++ README.md
@@ -14,4 +14,10 @@ Add the following environment variables in a **.env** file over te root folder:
 APP_PORT=3000
 APP_NAME="Test API"
 NODE_ENV="development"
+
+DB_NAME="movies_db"
+DB_HOST=
+DB_PORT=
+DB_USERNAME=
+DB_PASSWORD=
 ```
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,13 @@
 require( `dotenv` ).config();
 
 const config = {
-	APP_PORT : process.env.APP_PORT || 300,
-	NODE_ENV : process.env.NODE_ENV || `development`,
+	APP_PORT    : process.env.APP_PORT || 3000,
+	NODE_ENV    : process.env.NODE_ENV || `development`,
+	DB_NAME     : process.env.DB_NAME || ``,
+	DB_HOST     : process.env.DB_HOST || ``,
+	DB_PORT     : process.env.DB_PORT || ``,
+	DB_USERNAME : process.env.DB_USERNAME || ``,
+	DB_PASSWORD : process.env.DB_PASSWORD || ``,
 };
 
 module.exports = config;
